a8ac25bf691f5c121c8b67a3bdff8b42f2a93737
Petr <sppetrak@mail.ru>
2022-12-03 18:13:55 +0300

    1 v1.1

--- 1/Program.cs
+++ 1/Program.cs
@@ -2,34 +2,32 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 //[345, 897, 568, 234] -> 2
 
-int[] numbers = new int[10];
+int[] array = new int[10];
 
-FillArrayRandomNumbers(numbers);
+FillArrayRandomNumbers(array);
 Console.WriteLine("Массив трехзначных чисел");
-PrintArray(numbers);
+PrintArray(array);
 
 int count = 0;
 
-for (int a = 0; a < numbers.Length; a++)
-    if (numbers[a] % 2 == 0)
+for (int a = 0; a < array.Length; a++)
+    if (array[a] % 2 == 0)
         count++;
 
 Console.WriteLine($"количество четных чисел: {count}");
 
-void FillArrayRandomNumbers(int[] numbers)
+void FillArrayRandomNumbers(int[] array)
 {
-    for (int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        numbers[i] = new Random().Next(100, 1000);
+        array[i] = new Random().Next(100, 1000);
     }
 }
-void PrintArray(int[] numbers)
+void PrintArray(int[] array)
 {
-    Console.Write("[ ");
-    for (int i = 0; i < numbers.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write(array[i] + " ");
     }
-    Console.Write("]");
     Console.WriteLine();
 }
